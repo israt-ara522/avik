42fe7ee0f68eebc4e2eadac91d8f58e13fbdc97b
israt-ara522 <117897825+israt-ara522@users.noreply.github.com>
2022-11-10 14:18:07 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,134 @@
+const express = require('express')
+const cors = require("cors")
+
+const { MongoClient, ServerApiVersion, MongoRuntimeError, ObjectId } = require('mongodb');
+
+require('dotenv').config();
+const app = express()
+
+//port declare
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+// const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xfbrdjn.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+
+//mongodb part
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xfbrdjn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+
+    try {
+        const serviceCollection = client.db('avijatrik').collection('services');
+        const reviewCollection = client.db('avijatrik').collection('reviews');
+
+
+        //limited data for home page
+        app.get('/homedata', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.limit(3).toArray();
+            res.send(services)
+        })
+        //crud read operation for all the data
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        //crud read operation for specific data
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+
+        //crud operation for create new service
+        app.post('/services', async (req, res) => {
+
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result)
+
+        })
+
+        //review 
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews)
+        })
+        app.get('/reviews/:id', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews)
+        })
+
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result)
+        })
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+    }
+
+    finally {
+
+    }
+
+
+
+
+}
+
+run().catch(err => console.error(err))
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('my project is running')
+})
+
+app.listen(port, () => {
+    console.log(`my project is running on ${port}`)
+})
+
